5c118509b3747e47e95bb486279325e45fac96ae
senimamsistem <senimamsistem@gmail.com>
2024-07-09 08:55:49 +0000

    wallet-generate-EVM

--- wallet-generator/index.js
+++ wallet-generator/index.js
@@ -45,24 +45,20 @@ async function main() {
   for (let i = 0; i < numWallets; i++) {
     const { address, privateKey, mnemonic } = await createWallet(wordCount);
     const walletDetails = `Wallet ${i + 1}:\nAddress: ${address}\nPrivate Key: ${privateKey}\nMnemonic Phrase: ${mnemonic}\n\n`;
-
-    // Tampilkan detail wallet di terminal dengan warna
+    
     console.log(chalk.blue(`Wallet ${i + 1}:`));
     console.log(chalk.white('Address: ') + chalk.gray(address));
     console.log(chalk.white('Private Key: ') + chalk.magenta(privateKey));
     console.log(chalk.white('Mnemonic Phrase: ') + chalk.green(mnemonic));
     console.log('\n');
 
-    // Tambahkan detail wallet ke file wallet2.txt
     await fs.appendFile('wallet2.txt', walletDetails);
 
-    // Cek apakah address dimulai dengan 0x0000, 0x00000, atau 0x000000
     if (address.startsWith('0x0000') || address.startsWith('0x00000') || address.startsWith('0x000000')) {
       await fs.appendFile('wallet_main.txt', walletDetails);
       console.log(chalk.red(`Dompet ditemukan dengan alamat berawalan '0x0000', '0x00000', atau '0x000000':\nAlamat: ${address}\nKunci Pribadi: ${privateKey}`));
     }
 
-    // Cek apakah address dimulai dengan 0x9999 atau 0x99999
     if (address.startsWith('0x9999') || address.startsWith('0x99999')) {
       await fs.appendFile('wallet99.txt', walletDetails);
       console.log(chalk.yellow(`Dompet ditemukan dengan alamat berawalan '0x9999' atau '0x99999':\nAlamat: ${address}\nKunci Pribadi: ${privateKey}`));
